11cea78238635d8908bd63a3dd79dc2550647ff4
Jiapan Wei <wei.jiap@northeastern.edu>
2024-02-16 01:39:25 -0800

    Create vpc-module to config vpc setting

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 module "vpc1" {
-  source     = "./vpc-module"
-  project_id = var.project_id
-  region     = var.region
-  vpc_name = "vpc-1"
+  source             = "./vpc-module"
+  project_id         = var.project_id
+  region             = var.region
+  vpc_name           = "vpc-1"
   webapp_subnet_name = "webapp-1"
-  db_subnet_name = "db-1"
-  vpc_route_name = "route-to-internet-1"
+  db_subnet_name     = "db-1"
+  vpc_route_name     = "route-to-internet-1"
 }
 
 module "vpc2" {
-  source     = "./vpc-module"
-  project_id = var.project_id
-  region     = var.region
-  vpc_name = "vpc-2"
+  source             = "./vpc-module"
+  project_id         = var.project_id
+  region             = var.region
+  vpc_name           = "vpc-2"
   webapp_subnet_name = "webapp-2"
-  db_subnet_name = "db-2"
-  vpc_route_name = "route-to-internet-2"
+  db_subnet_name     = "db-2"
+  vpc_route_name     = "route-to-internet-2"
 }
