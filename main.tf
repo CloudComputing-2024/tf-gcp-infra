faa65523be38fe8e20f8cd2b542b72001c34076a
Jiapan Wei <wei.jiap@northeastern.edu>
2024-02-16 02:16:54 -0800

    Update modlue vpc in  main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,9 @@
-module "vpc1" {
+module "vpc" {
   source             = "./vpc-module"
   project_id         = var.project_id
   region             = var.region
-  vpc_name           = "vpc-1"
-  webapp_subnet_name = "webapp-1"
-  db_subnet_name     = "db-1"
-  vpc_route_name     = "route-to-internet-1"
-}
-
-module "vpc2" {
-  source             = "./vpc-module"
-  project_id         = var.project_id
-  region             = var.region
-  vpc_name           = "vpc-2"
-  webapp_subnet_name = "webapp-2"
-  db_subnet_name     = "db-2"
-  vpc_route_name     = "route-to-internet-2"
+  vpc_name           = "vpc"
+  webapp_subnet_name = "webapp"
+  db_subnet_name     = "db"
+  vpc_route_name     = "route-to-internet"
 }
