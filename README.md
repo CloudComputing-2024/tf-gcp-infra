650fd9eceb63ddc168061c1d8ad3e0dde6de8031
Jiapan Wei <wei.jiap@northeastern.edu>
2024-02-16 16:44:46 -0800

    Update terraform configs

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# tf-gcp-infra
+
+## Infrastructure Setup Instructions
+
+1. **Create Virtual Private Cloud (VPC) with the following requirements:**
+    - Auto-create subnets should be disabled.
+    - Routing mode should be set to regional.
+    - No default routes should be created.
+
+2. **Create subnets in VPC:**
+    - There are 2 subnets in the VPC, the first one named `webapp` and the second one named `db`.
+    - Each subnet has a /24 CIDR address range.
+    - Add a route explicitly to `0.0.0.0/0` with the next hop to the Internet Gateway and attach it to your VPC.
--- main.tf
+++ main.tf
@@ -1,9 +1,29 @@
-module "vpc" {
-  source             = "./vpc-module"
-  project_id         = var.project_id
-  region             = var.region
-  vpc_name           = "vpc"
-  webapp_subnet_name = "webapp"
-  db_subnet_name     = "db"
-  vpc_route_name     = "route-to-internet"
+resource "google_compute_network" "vpc_network" {
+  name                            = var.vpc_name
+  project                         = var.project_id
+  auto_create_subnetworks         = false
+  routing_mode                    = "REGIONAL"
+  delete_default_routes_on_create = true
+}
+
+resource "google_compute_subnetwork" "webapp_subnet" {
+  name          = var.webapp_subnet_name
+  ip_cidr_range = var.webapp_subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_subnetwork" "db_subnet" {
+  name          = var.db_subnet_name
+  ip_cidr_range = var.db_subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_route" "default_route_to_internet" {
+  name             = var.vpc_route_name
+  dest_range       = var.vpc_route_dest_range
+  network          = google_compute_network.vpc_network.id
+  next_hop_gateway = "default-internet-gateway"
+  priority         = var.vpc_route_priority
 }
