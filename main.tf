74709e0d01ad22a8b21e8e60a94ee4615b8dfbaa
jiapanWei <113729007+jiapanWei@users.noreply.github.com>
2024-02-21 22:13:52 -0800

    Update main.tf to create instance (#11)

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "google_compute_network" "vpc_network" {
   name                            = var.vpc_name
   project                         = var.project_id
   auto_create_subnetworks         = false
-  routing_mode                    = "REGIONAL"
+  routing_mode                    = var.routing_mode
   delete_default_routes_on_create = true
 }
 
@@ -24,6 +24,41 @@ resource "google_compute_route" "default_route_to_internet" {
   name             = var.vpc_route_name
   dest_range       = var.vpc_route_dest_range
   network          = google_compute_network.vpc_network.id
-  next_hop_gateway = "default-internet-gateway"
+  next_hop_gateway = var.next_hop_gateway
   priority         = var.vpc_route_priority
+}
+
+resource "google_compute_firewall" "allow_traffic" {
+  name    = var.firewall_name
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = var.protocol_name
+    ports    = var.application_port
+  }
+  source_ranges = var.firewall_source_ranges
+  target_tags   = var.firewall_tags
+}
+
+resource "google_compute_instance" "vm_instance" {
+  machine_type = var.machine_type
+  name         = var.instance_name
+  zone         = var.instance_zone
+
+  boot_disk {
+    initialize_params {
+      image = var.boot_disk_image
+      type  = var.boot_disk_type
+      size  = var.boot_disk_size
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.webapp_subnet.id
+
+    access_config {}
+  }
+
+  tags = [var.firewall_name]
 }
--- variables.tf
+++ variables.tf
@@ -1,53 +1,98 @@
 variable "project_id" {
-  description = "The project id of GCP project"
-  type        = string
+  type = string
 }
 
 variable "region" {
-  description = "The region of resources be created."
-  type        = string
+  type = string
 }
 
 variable "vpc_name" {
-  description = "The name of vpc network."
-  type        = string
+  type = string
+}
+
+variable "routing_mode" {
+  type = string
+}
+
+variable "firewall_name" {
+  type = string
+}
+
+variable "protocol_name" {
+  type = string
+}
+
+variable "application_port" {
+  type    = list(string)
+  default = ["8080"]
+}
+
+variable "firewall_source_ranges" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
 }
 
 variable "webapp_subnet_name" {
-  description = "The name of the subnet for webapp."
-  type        = string
+  type = string
 }
 
 variable "webapp_subnet_cidr" {
-  description = "The CIDR for webapp subnet."
-  type        = string
-  default     = "10.0.0.0/24"
+  type    = string
+  default = "10.0.0.0/24"
 }
 
 variable "db_subnet_name" {
-  description = "The name of subnet for the databases."
-  type        = string
+  type = string
 }
 
 variable "db_subnet_cidr" {
-  description = "The CIDR for db subnet."
-  type        = string
-  default     = "10.0.1.0/24"
+  type    = string
+  default = "10.0.1.0/24"
 }
 
 variable "vpc_route_name" {
-  description = "The name of the route for vpc."
-  type        = string
+  type = string
 }
 
 variable "vpc_route_priority" {
-  description = "The priority of the route."
-  type        = number
-  default     = 100
+  type    = number
+  default = 100
 }
 
 variable "vpc_route_dest_range" {
-  description = "The destination range for the route-to-internet"
-  type        = string
-  default     = "0.0.0.0/0"
+  type    = string
+  default = "0.0.0.0/0"
 }
+
+variable "firewall_tags" {
+  type = list(string)
+}
+
+variable "next_hop_gateway" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "instance_zone" {
+  type = string
+}
+
+variable "boot_disk_image" {
+  type = string
+}
+
+variable "boot_disk_type" {
+  type = string
+}
+
+variable "boot_disk_size" {
+  type = number
+}
+
