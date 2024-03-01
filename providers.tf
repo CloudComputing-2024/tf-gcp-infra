95fb307cae69ffc28f0541d28fd579fe1eeaf235
Jiapan Wei <wei.jiap@northeastern.edu>
2024-03-01 15:33:13 -0800

    Upate terraform build instance

--- providers.tf
+++ providers.tf
@@ -2,3 +2,10 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+provider "google-beta" {
+  region = "us-central1"
+  zone   = "us-central1-a"
+}
+
+provider "random" {}
